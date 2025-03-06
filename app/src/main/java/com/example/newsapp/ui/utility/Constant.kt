1c1cd187a04b894eb28cac8c369bde5b3db3184d
Sudarshan HS <send2sudarshanhs@gmail.com>
2025-03-06 15:07:48 +0530

    code cleaned

--- app/src/main/java/com/example/newsapp/ui/utility/Constant.kt
+++ app/src/main/java/com/example/newsapp/ui/utility/Constant.kt
@@ -1,7 +1,7 @@
 package com.example.newsapp.ui.utility
 
 object Constants {
-    const val API_KEY = "a14c994ddfa74c17850161c6abf57a83"
+    const val API_KEY = "APK-KEY-Here"
     const val QUERY = "tesla"
     const val FROM_DATE = "2025-02-05"
     const val SORT_BY = "publishedAt"
